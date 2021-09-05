130cf8bf24a6f437510ebdb8d1f4d2e9e77f6483
gtiwari1999 <tiwarig505@gmail.com>
2021-09-05 16:17:22 +0530

    Updates

---
+++ precache-manifest.840e31d6a01895ddf70c2b04728ebfb7.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "174bacee2d54b8b88b5f84f8428146ae",
+    "url": "/weather-react/index.html"
+  },
+  {
+    "revision": "0f54d52807122c342de4",
+    "url": "/weather-react/static/css/1.b07a53c4.chunk.css"
+  },
+  {
+    "revision": "24c3574d007bcc7c5f9a",
+    "url": "/weather-react/static/css/6.bcf52850.chunk.css"
+  },
+  {
+    "revision": "20ae753d93bc1a97f9ab",
+    "url": "/weather-react/static/css/main.339d9806.chunk.css"
+  },
+  {
+    "revision": "6b8f269992ea2bfff341",
+    "url": "/weather-react/static/js/0.2673a025.chunk.js"
+  },
+  {
+    "revision": "97636669c8193ace9a3b4f1548fc4d0b",
+    "url": "/weather-react/static/js/0.2673a025.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f54d52807122c342de4",
+    "url": "/weather-react/static/js/1.0227921d.chunk.js"
+  },
+  {
+    "revision": "0d341ab4e10e03a4a4b3",
+    "url": "/weather-react/static/js/4.b211d283.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/weather-react/static/js/4.b211d283.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1104b9c1b472bdb58d26",
+    "url": "/weather-react/static/js/5.248d5f78.chunk.js"
+  },
+  {
+    "revision": "24c3574d007bcc7c5f9a",
+    "url": "/weather-react/static/js/6.d6bcc332.chunk.js"
+  },
+  {
+    "revision": "3426e14d948658c07887",
+    "url": "/weather-react/static/js/7.1580049f.chunk.js"
+  },
+  {
+    "revision": "a8f5d8a38cb7d7e922ac",
+    "url": "/weather-react/static/js/8.e6c621d4.chunk.js"
+  },
+  {
+    "revision": "a5c9b72a018b38ac79a0",
+    "url": "/weather-react/static/js/9.8fb47210.chunk.js"
+  },
+  {
+    "revision": "20ae753d93bc1a97f9ab",
+    "url": "/weather-react/static/js/main.e532da4a.chunk.js"
+  },
+  {
+    "revision": "dfc563dc86ad2fbbcb2c",
+    "url": "/weather-react/static/js/runtime-main.65d57a46.js"
+  },
+  {
+    "revision": "788e7c705c377d9e08875341f0e860cb",
+    "url": "/weather-react/static/media/SpaceGrotesk-Bold.788e7c70.woff2"
+  },
+  {
+    "revision": "7b9e40f6db710fa59344e5dc8cc34539",
+    "url": "/weather-react/static/media/SpaceGrotesk-Bold.7b9e40f6.woff"
+  },
+  {
+    "revision": "ff7cc358afba9c9f9dbfe590da86fdc4",
+    "url": "/weather-react/static/media/SpaceGrotesk-Bold.ff7cc358.ttf"
+  },
+  {
+    "revision": "3564bf8ea8fd95ef7fc035ccb3dcc6bd",
+    "url": "/weather-react/static/media/SpaceGrotesk-Light.3564bf8e.ttf"
+  },
+  {
+    "revision": "3a844470ad64cd09f292369c7841ac9a",
+    "url": "/weather-react/static/media/SpaceGrotesk-Light.3a844470.woff2"
+  },
+  {
+    "revision": "f5c67c0c4c54897c8b64ac0957fdc364",
+    "url": "/weather-react/static/media/SpaceGrotesk-Light.f5c67c0c.woff"
+  },
+  {
+    "revision": "3d44559f46b9069844ce17bcf967f264",
+    "url": "/weather-react/static/media/SpaceGrotesk-Medium.3d44559f.ttf"
+  },
+  {
+    "revision": "61a3f6134b0f3a5eb11b6b740ffcabaa",
+    "url": "/weather-react/static/media/SpaceGrotesk-Medium.61a3f613.woff"
+  },
+  {
+    "revision": "ed1a889bb737857a86acd44abfc1a9f4",
+    "url": "/weather-react/static/media/SpaceGrotesk-Medium.ed1a889b.woff2"
+  },
+  {
+    "revision": "5529b729a77573fc6130f2db44a19c77",
+    "url": "/weather-react/static/media/SpaceGrotesk-Regular.5529b729.woff2"
+  },
+  {
+    "revision": "982323eb635ecc1f9fc8d20e6bbd9316",
+    "url": "/weather-react/static/media/SpaceGrotesk-Regular.982323eb.ttf"
+  },
+  {
+    "revision": "fbbf6d98bfb9f3410a0de34728e9250e",
+    "url": "/weather-react/static/media/SpaceGrotesk-Regular.fbbf6d98.woff"
+  },
+  {
+    "revision": "0afbf8e46c88529a3f140bacd48f5949",
+    "url": "/weather-react/static/media/SpaceGrotesk-SemiBold.0afbf8e4.woff2"
+  },
+  {
+    "revision": "18970edf2f9900dc6928cdc0b64c348b",
+    "url": "/weather-react/static/media/SpaceGrotesk-SemiBold.18970edf.ttf"
+  },
+  {
+    "revision": "7b228459800eddb3e86861952daa4528",
+    "url": "/weather-react/static/media/SpaceGrotesk-SemiBold.7b228459.woff"
+  }
+]);
